9b91aa13f8c76bc6c69efa063bf1d3ec0d8eef54
madogiwa0124 <madogiwa0124@gmail.com>
2024-09-14 22:13:35 +0900

    add soucemap logger test.

--- src/sourceMap/logger.ts
+++ src/sourceMap/logger.ts
@@ -1,9 +1,29 @@
 export const loggedNoSourceFound = (sourceMapPath: string): null => {
-  console.error(`No source found for '${sourceMapPath}'`, true);
+  console.error(`No source found for '${sourceMapPath}'.`);
   return null;
 };
 
 export const loggedReadErrorSourceMap = (sourceMapPath: string): null => {
-  console.error(`Error reading sourcemap file: ${sourceMapPath}`, true);
+  console.error(`Error reading sourcemap file: ${sourceMapPath}`);
   return null;
 };
+
+if (import.meta.vitest) {
+  const { describe, it, expect, vi } = import.meta.vitest;
+
+  describe("loggedNoSourceFound", () => {
+    it("should log no source found", () => {
+      vi.spyOn(console, "error").mockImplementation(() => {});
+      loggedNoSourceFound("foo.js.map");
+      expect(console.error).toBeCalledWith("No source found for 'foo.js.map'.");
+    });
+  });
+
+  describe("loggedReadErrorSourceMap", () => {
+    it("should log read error source map", () => {
+      vi.spyOn(console, "error").mockImplementation(() => {});
+      loggedReadErrorSourceMap("foo.js.map");
+      expect(console.error).toBeCalledWith("Error reading sourcemap file: foo.js.map");
+    });
+  });
+}
